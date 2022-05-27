1e47e4121e203ceb02c3486dbc47960b835fc11b
ciel <0329cherry@naver.com>
2022-05-27 17:10:00 +0900

    진짜 죄송해요

--- Project3/src/main/java/com/spring/mall/cart/controller/CartController.java
+++ Project3/src/main/java/com/spring/mall/cart/controller/CartController.java
@@ -2,19 +2,20 @@ package com.spring.mall.cart.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/cart/**")
 public class CartController {
 
 	public CartController() {
 		System.out.println(">> CartController() 객체 생성 ");
 	}
 	
-	@GetMapping
+	@GetMapping("cart.do")
 	public String cartView() {
-		System.out.println("장바구니페이지(cart.jsp)이동 - cartView()");
-		
+		System.out.println("장바구니 페이지(cart.jsp)이동 - cartView()");
 		return "user/cart";
 	}
 
