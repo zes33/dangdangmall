2b6a7921bd9b6c155a8f9b39206c699ece411537
jiyoung <positive246@naver.com>
2022-05-27 15:25:00 +0900

    commit

---
+++ Project3/src/main/java/com/spring/mall/product/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.spring.mall.product.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController{
+	
+	public MainController() {
+		System.out.println("MainController() 객체 생성");
+	}
+	
+	@GetMapping
+	public String mainView() {
+		System.out.println("메인페이지(main.jsp)이동 - mainView()");
+		
+		return "store/main";
+	}
+
+}
