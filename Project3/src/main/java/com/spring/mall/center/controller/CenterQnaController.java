95a65f6e2c8ff8fb7ce936241eae5a0cae445902
jin <cdmvjtl@naver.com>
2022-05-31 18:23:41 +0900

    CenterQna 컨트롤러, 매퍼, admin/getCenterList 수정!~

--- Project3/src/main/java/com/spring/mall/center/controller/CenterQnaController.java
+++ Project3/src/main/java/com/spring/mall/center/controller/CenterQnaController.java
@@ -8,19 +8,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.spring.mall.center.service.CenterReplyService;
-import com.spring.mall.center.vo.CenterReplyVO;
+import com.spring.mall.center.service.CenterQnaService;
+import com.spring.mall.center.vo.CenterQnaVO;
 
 @Controller
 @RequestMapping("/admin/**")
-public class CenterReplyController {
+public class CenterQnaController {
 	@Autowired
-	private CenterReplyService centerReplyService;
+	private CenterQnaService centerQnaService;
 	
-	public CenterReplyController() {
-		System.out.println("======= CenterReplyController() 객체 생성~~");
+	public CenterQnaController() {
+		System.out.println("======= CenterQnaController() 객체 생성~~");
 	}
 	
+	@GetMapping("/adminGetCenterList.do")
+	public String adminGetCenterList(Model model) {
+		CenterQnaVO vo = null;
+		List<CenterQnaVO> adminGetCenterList = centerQnaService.getCenterQnaList(vo);
+		model.addAttribute("adminGetCenterList", adminGetCenterList);
+		
+		System.out.println("관리자 고객 문의 목록 페이지(getCenterList.jsp)이동 - adminGetCenterList()");
+		
+		return "admin/getCenterList";
+	}
 	
 //	
 //	@GetMapping("/adminGetProductList.do")
